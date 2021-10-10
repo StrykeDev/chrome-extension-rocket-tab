92439e9a8d78a2706d1652df1e697053419c9eff
StrykeDev <attias.barak@gmail.com>
2021-10-10 17:05:18 +0300

    Fixed the clock

---
+++ scripts/clock.js
@@ -0,0 +1,23 @@
+
+
+const clock = document.getElementById('clock');
+
+function ClockTick() {
+    let date = new Date();
+    let h = date.getHours()
+    let m = date.getMinutes()
+    let symbol = 'AM';
+
+    if (h == 0) {
+        h = 12
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        symbol = "PM";
+    }
+
+    clock.innerText = `${('0' + h).slice(-2)}:${('0' + m).slice(-2)} ${symbol}`;
+}
+
+setInterval(() => ClockTick(), 1000);
