0eab77d7ae0b330258c19171ea05fa4d10a27b38
StrykeDev <attias.barak@gmail.com>
2021-10-13 19:09:41 +0300

    Improved the background options

---
+++ assets/backgrounds/backgrounds.js
@@ -0,0 +1,42 @@
+
+const width = 1920;
+const height = 1080;
+
+export default [
+    {
+        "name": "Development",
+        "url": "../assets/backgrounds/photo-1522252234503-e356532cafd5.jpg",
+    },
+    {
+        "name": "Dog",
+        "url": `https://images.unsplash.com/photo-1611003228941-98852ba62227?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&q=80&w=${width}&h=${height}`
+    },
+    {
+        "name": "Frozen ocean",
+        "url": `https://images.unsplash.com/photo-1547994770-e5d8509b114d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=100&w=${width}&h=${height}`,
+    },
+    {
+        "name": "Tropical beach",
+        "url": `https://images.unsplash.com/photo-1548041347-390744c58da6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=100&w=${width}&h=${height}`,
+    },
+    {
+        "name": "Iceland waterfall",
+        "url": `https://images.unsplash.com/photo-1548049983-5be60c9bc004?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&q=100&w=${width}&h=${height}`,
+    },
+    {
+        "name": "Forest",
+        "url": `https://images.unsplash.com/photo-1473448912268-2022ce9509d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&q=100&w=${width}&h=${height}`,
+    },
+    {
+        "name": "Sunset beach",
+        "url": `https://images.unsplash.com/photo-1547532182-bf296f6be875?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&q=100&w=${width}&h=${height}`,
+    },
+    {
+        "name": "Sunset forest",
+        "url": `https://images.unsplash.com/photo-1547989453-11e67ffb3885?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&q=100&w=${width}&h=${height}`,
+    },
+    {
+        "name": "Sunset path",
+        "url": `https://images.unsplash.com/photo-1496614932623-0a3a9743552e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&q=100&w=${width}&h=${height}`,
+    }
+]
--- js/background.js
+++ js/background.js
@@ -1,30 +1,14 @@
 
+import backgrounds from "../assets/backgrounds/backgrounds.js";
 const backgroundList = document.getElementById("background-list");
-const width = 1280; // 1680 recommanded
-const backgrounds = [
-    {
-        url: "https://images.unsplash.com/photo-1611003228941-98852ba62227?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&q=80&w=" + width
-    },
-    {
-        url: "https://images.unsplash.com/photo-1547532182-bf296f6be875?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&q=100&w=" + width,
-    },
-    {
-        url: "https://images.unsplash.com/photo-1548041347-390744c58da6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=100&w=" + width,
-    },
-    {
-        url: "https://images.unsplash.com/photo-1548049983-5be60c9bc004?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&q=100&w=" + width,
-    },
-    {
-        url: "https://images.unsplash.com/photo-1547994770-e5d8509b114d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=100&w=" + width,
-    },
-    {
-        url: "https://images.unsplash.com/photo-1547989453-11e67ffb3885?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&q=100&w=" + width,
-    },
-];
 
-function BackgroundSelected(backgroundIndex) {
+// TODO: Added fade effect
+function BackgroundSelected(backgroundIndex, loaded = true) {
     document.body.style.backgroundImage = `url(${backgrounds[backgroundIndex].url})`;
-    chrome.storage.local.set({ 'selectedBackground': backgroundIndex });
+
+    if (loaded) {
+        chrome.storage.local.set({ 'selectedBackground': backgroundIndex });
+    }
 }
 
 function ClearBackground() {
@@ -37,32 +21,32 @@ backgrounds.forEach((background, index) => {
     const button = document.createElement("button");
     button.type = "button";
     button.className = "background-item btn drop-shadow";
-    button.onclick = () => BackgroundSelected(index);
     button.innerHTML = 'Loading...'
     button.disabled = true;
+    button.addEventListener("click", () => BackgroundSelected(index));
     backgroundList.appendChild(button);
 
     const img = new Image();
     img.src = background.url
-    img.onload = () => {
+    img.addEventListener('load', () => {
         button.style.backgroundImage = `url(${background.url})`;
         button.innerText = ''
         button.disabled = false;
-    }
+    })
 });
 
 const button = document.createElement("button");
 button.type = "button";
 button.className = "background-item btn drop-shadow";
-button.onclick = () => ClearBackground();
 button.innerHTML = 'No Background'
 button.style.background = 'linear-gradient(45deg, #d972f3 ,#3764eb)'
+button.addEventListener("click", () => ClearBackground());
 backgroundList.appendChild(button);
 
 
 // Load last background from storage
 chrome.storage.local.get('selectedBackground', (result) => {
     if (result.selectedBackground != undefined) {
-        document.body.style.backgroundImage = `url(${backgrounds[result.selectedBackground].url})`;
+        BackgroundSelected(result.selectedBackground, false);
     }
 })
